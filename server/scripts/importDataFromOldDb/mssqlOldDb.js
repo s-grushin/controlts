cc257829c774143f1a7c3f7ba375d7dacf8c1f6c
S.Grushin <Zoomec@vikas.com.ua>
2022-12-08 22:41:39 +0200

    fixed connection string

--- server/scripts/importDataFromOldDb/mssqlOldDb.js
+++ server/scripts/importDataFromOldDb/mssqlOldDb.js
@@ -11,12 +11,13 @@ const mssqlOldDb = new Sequelize({
     database: process.env.MSSQL_OLD_DB_DBNAME,
     dialectOptions: {
         options: {
+            instanceName: process.env.MSSQL_OLD_DB_INSTANCENAME,
             encrypt: false,
             enableArithAbort: false,
             cryptoCredentialsDetails: {
                 minVersion: 'TLSv1'
             }
-        }
+        },        
     },
     logging: false,
 
